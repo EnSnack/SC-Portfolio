da93effefd8feffcf102c652e3fc329462d9f7d2
EnSnack <diabloste@gmail.com>
2022-10-17 15:50:01 +0200

    Move words to seperate file and load file

---
+++ js/words.js
@@ -0,0 +1,62 @@
+var _WORDS_JSON = {
+  "Adjectives": [
+    {
+      "en": "modern",
+      "dk": "moderne"
+    },
+    {
+      "en": "enthusiastic",
+      "dk": "entusiastisk"
+    },
+    {
+      "en": "versatile",
+      "dk": "alsidig"
+    },
+    {
+      "en": "astute",
+      "dk": "kløgtig"
+    },
+    {
+      "en": "skilled",
+      "dk": "dygtig"
+    },
+    {
+      "en": "p",
+      "dk": "kompetent"
+    },
+    {
+      "en": "keen",
+      "dk": "ivrig"
+    },
+    {
+      "en": "systematic",
+      "dk": "systematisk"
+    },
+    {
+      "en": "passionate",
+      "dk": "passioneret"
+    }
+  ],
+  "Nouns": [
+    {
+      "en": "engineer",
+      "dk": "ingeniør"
+    },
+    {
+      "en": "teacher",
+      "dk": "lærer"
+    },
+    {
+      "en": "learner",
+      "dk": "lærende"
+    },
+    {
+      "en": "leader",
+      "dk": "leder"
+    },
+    {
+      "en": "programmer",
+      "dk": "programmør"
+    }
+  ]
+}
