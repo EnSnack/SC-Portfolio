2d577e83110c631e37a90b3689748b79d7c4244e
EnSnack <diabloste@gmail.com>
2022-10-17 14:23:58 +0200

    Initial Files

---
+++ js/basic.js
@@ -0,0 +1,260 @@
+/*
+	Typewriter Style Text
+*/
+const _WORDS_JSON = { "Adjectives": [{"en":"modern","dk":"moderne"},{"en":"enthusiastic","dk":"entusiastisk"},{"en":"versatile","dk":"alsidig"},{"en":"astute","dk":"kløgtig"},{"en":"skilled","dk":"dygtig"},{"en":"p","dk":"kompetent"},{"en":"keen","dk":"ivrig"},{"en":"systematic","dk":"systematisk"},{"en":"passionate","dk":"passioneret"}], "Nouns": [{"en": "engineer", "dk": "ingeniør"},{"en": "teacher", "dk": "lærer"},{"en":"learner", "dk": "lærende"},{"en": "leader", "dk": "leder"},{"en": "programmer", "dk": "programmør"}]}
+let TypeWriterActive = false;
+let CurrentActiveLanguage = 'en';
+
+/**
+ * Writes text similar to that of a typewriter.
+ *
+ * @param {string} id The ID of the element to edit.
+ * @param {string} word The word to write.
+ * @param {number} dspeed Speed in milliseconds of erasing every individual letter, defaults to 150.
+ * @param {number} wspeed Speed in milliseconds of writing every individual letter, defaults to 150.
+ * @return {boolean} Whether successful or not.
+ */
+function write(id,word,dspeed,wspeed) {
+	return new Promise((resolve) => {
+		const element = document.querySelector(`[lang="${CurrentActiveLanguage}"][id="${id}"]`)
+		
+		// Failsafe
+		if(TypeWriterActive || word.length <= 0 || !isNaN(word) || word.length > 16) return;
+		
+		blinkingCursor(element)
+		
+		TypeWriterActive = true;
+		
+		// Erasing our current word
+		let erase = setInterval(function() {
+			if(element.innerHTML.length == 0) {
+				clearInterval(erase)
+				erase = null;
+				// Hard-coded exceptions for the English language.
+				// Just checking for the letter isn't going to work, A vs An is based on sound, not letter.
+				if(CurrentActiveLanguage == "en" && id == "what-adj") {
+					const _EXCEPTIONS = ["enthusiastic","astute"]
+					document.getElementById("what-pre").innerHTML = _EXCEPTIONS.includes(word) ? "An" : "A"
+				}
+			}
+			if(element.innerHTML.length > 0) {
+				element.innerHTML = element.innerHTML.substring(0,element.innerHTML.length-1)
+			}
+		},(!isNaN(dspeed) && dspeed > 0 ? dspeed : 150))
+		
+		// Writing our new word
+		let i=0;
+		let write = setInterval(function() {
+			if(element.innerHTML == word) {
+				clearInterval(write);
+				write = null;
+				TypeWriterActive = false
+				resolve(true);
+			}
+			if(element.innerHTML.length < word.length && erase == null) {
+				element.innerHTML+=word.charAt(i++)
+			}
+		},(!isNaN(wspeed) && wspeed > 0 ? wspeed : 150))
+	})
+}
+
+/**
+ * Adds a blinking cursor similar to that of a word document or otherwise.
+ *
+ * @param {element} e The element to move the cursor to.
+ */
+function blinkingCursor(e) {
+	let cursor = document.querySelector("#what-cursor");
+	e.after(cursor);
+}
+
+/**
+ * A dot function for formatting custom inline functions for projects page.
+ *
+ * @param {o} json Our current json object.
+ * @return {string} Formatted string.
+ */
+String.prototype.format = function(o) {
+	return this
+		   // Format \n -> <br> ; Newlines
+		   .replace(/\n/g, "<br>")
+		   // Format %ls -> <a href=<object_link>> ; Object Link Start
+		   .replace(/\%ls/g, `<a href='${o['link']}' target='_blank'>`)
+		   // Format %as[<link>] -> <a href=<link>> ; Link content
+		   .replace(/\%as\[(.*?)\]/g, `<a href='$1' target='_blank'>`)
+		   // Format %ae -> </a> ; Link end
+		   .replace(/\%ae/g, `</a>`)
+};
+
+/**
+ * A recursive projects page populator.
+ *
+ * @param {json} json Our JSON file.
+ */
+function populateProjects(json) {
+	if(Object.keys(json.Projects).length == 0) return;
+	const _FIRST_OBJECT = json.Projects[Object.keys(json.Projects)[0]]
+	if(document.querySelector("div[class^=row-]:last-child") === null || document.querySelector("div[class^=row-]:last-child").length === 0) {
+		document.querySelector("#portfolio .content").innerHTML += "<div class='row-1' style='display:flex;flex-direction:row;'></div>"
+	} else if(document.querySelectorAll("div[class^=row-]:last-child > *").length >= 4) {
+		document.querySelector("#portfolio .content").innerHTML += `<div class='row-${parseInt(document.querySelector("div[class^=row-]:last-child").classList[0].split("-")[1])+1}' style='display:flex;flex-direction:row;'></div>`
+	}
+	const _CURRENT_ROW      = document.querySelector("div[class^=row-]:last-child")
+	let _CURRENT_BOX        = "<div class='contentbox'></div>"
+	_CURRENT_ROW.innerHTML += _CURRENT_BOX
+	_CURRENT_BOX            = _CURRENT_ROW.querySelector(".contentbox:last-child")
+	
+	// No longer does, I have to do unique code execution based on type :(
+	//* Rids us some code duplication.
+	//const _TYPES            = ['name','','body']
+	if('name' in _FIRST_OBJECT) {
+		_CURRENT_BOX.innerHTML += `<div class='box-header'>${_FIRST_OBJECT['name'][0]}</div>`
+		// Despite this checking for a length bigger than 1, we only support 2 languages.
+		if(_FIRST_OBJECT['body'].length > 1) {
+			_CURRENT_BOX.querySelector(`.box-header`).setAttribute("lang", "en")
+			_CURRENT_BOX.innerHTML += `<div class='box-header' lang="dk">${_FIRST_OBJECT['name'][1]}</div>`
+		}
+	}
+	if('images' in _FIRST_OBJECT) {
+		// Add carousel if we have multiple images, else don't.
+		if(_FIRST_OBJECT['images'].length > 1) {
+			_CURRENT_BOX.innerHTML += `<div class='box-image'><section id="image-carousel" class="splide" aria-label="${_FIRST_OBJECT['name']}"><div class="splide__track"><ul class="splide__list"></ul></div></section></div>`
+			for (const property in _FIRST_OBJECT['images']) {
+				_CURRENT_BOX.querySelector(".splide__list").innerHTML += `<li class="splide__slide"><img src="${_FIRST_OBJECT['images'][property]}"></li>`
+			}
+		} else {
+			_CURRENT_BOX.innerHTML += `<div class='box-image' aria-label="${_FIRST_OBJECT['name']}"></div>`
+			_CURRENT_BOX.querySelector(".box-image").innerHTML += `<img src="${_FIRST_OBJECT['images'][0]}">`
+		}
+	}
+	if('body' in _FIRST_OBJECT) {
+		_CURRENT_BOX.innerHTML += `<div class='box-body'>${_FIRST_OBJECT['body'][0]}</div>`
+		// Despite this checking for a length bigger than 1, we only support 2 languages.
+		if(_FIRST_OBJECT['body'].length > 1) {
+			_CURRENT_BOX.querySelector(`.box-body`).setAttribute("lang", "en")
+			_CURRENT_BOX.innerHTML += `<div class='box-body' lang="dk">${_FIRST_OBJECT['body'][1]}</div>`
+		}
+		_CURRENT_BOX.querySelectorAll(`.box-body`).forEach((item) => item.innerHTML = item.innerHTML.format(_FIRST_OBJECT))
+	}
+	_CURRENT_BOX.innerHTML += `<div class="box-break"></div>`
+	if('footer' in _FIRST_OBJECT) {
+		if('date' in _FIRST_OBJECT['footer']) {
+			_CURRENT_BOX.innerHTML += `<div class="box-footer"><div class="box-footer-date">${_FIRST_OBJECT['footer']['date']}</div></div>`
+		}
+	}
+	
+	delete json.Projects[Object.keys(json.Projects)[0]]
+	populateProjects(json)
+}
+
+window.onload = function() {
+	const _NOUN_TIME    = 4000;
+	const _ADJ_TIME     = 1500;
+	const _CURSOR_BLINK = 500;
+	
+	// Honey, I'm home!
+	document.querySelector("#home").classList.toggle("section-show")
+	// Populate projects, this may take a second...
+	populateProjects(projects);
+	// Base language is English
+	document.querySelectorAll("body *[lang=dk]").forEach((node) => node.style.display = 'none');
+	
+	// Mount all image carousels, splide necessity.
+	new Splide( '#image-carousel' ).mount();
+	
+	let adjIndex=0;
+	let nounIndex=0;
+	setInterval(function() {
+		write("what-noun",_WORDS_JSON.Nouns[nounIndex][CurrentActiveLanguage],150,150).then((success) => {
+			if(success) {
+				nounIndex=nounIndex>=_WORDS_JSON.Nouns.length-1?0:nounIndex+1;
+			}
+		}).catch((e) => {
+			return console.error(e);
+		})
+	},_NOUN_TIME)
+	setInterval(function() {
+		write("what-adj",_WORDS_JSON.Adjectives[adjIndex][CurrentActiveLanguage],150,150).then((success) => {
+			if(success) {				
+				adjIndex=adjIndex>=_WORDS_JSON.Adjectives.length-1?0:adjIndex+1;
+			}
+		}).catch((e) => {
+			return console.error(e);
+		})
+	},_ADJ_TIME)
+	setInterval(function() {
+		if(!TypeWriterActive) {
+			document.querySelector("#what-cursor").style.display = (document.querySelector("#what-cursor").style.display == "none") ? "unset" : "none";
+		} else {
+			document.querySelector("#what-cursor").style.display = "unset";
+		}
+	},_CURSOR_BLINK)
+	
+	document.querySelectorAll("#navbar #navbox ul li a").forEach((item) => { item.addEventListener("click", function() {
+		if(item.classList[1] == "active") return;
+		const _CUR_ACTIVE = document.querySelector(".nav-link.active");
+		
+		_CUR_ACTIVE.classList.toggle("active");
+		document.querySelector(_CUR_ACTIVE.getAttribute("href")).classList.toggle("section-show");
+		document.querySelector(item.getAttribute("href")).classList.toggle("section-show");
+		item.classList.toggle("active");
+	})})
+
+	document.querySelectorAll(".btn-popup, #close").forEach((item) => { item.addEventListener("click", function (data) {
+			const _OUR_TARGET    = data.target.tagName == "IMG" ? findAncestor(data.target,".popup").id : `${data.target.id}-popup`;
+			const _POPUP_ELEMENT = document.querySelector(`#${_OUR_TARGET}`);
+			if(_POPUP_ELEMENT.classList.value.indexOf("popup-active") > -1) {
+				_POPUP_ELEMENT.querySelector(".popup-text-active").classList.toggle("popup-text-active");
+			} else {
+				_POPUP_ELEMENT.classList.toggle("popup-active");
+			}
+		});
+	});
+	
+	document.querySelector("#copy img").addEventListener("click", function () {
+		navigator.clipboard.writeText("wollausteffen@gmail.com");
+		document.querySelector(".popup-tooltip-box").classList.toggle("popup-tooltip-box-active");
+	});
+	
+	document.querySelector(".popup").addEventListener("transitionend", function (data) {
+		if(data.target.classList[0] == "popup" && data.target.classList.length == 2 && data.propertyName == "width") {
+			document.querySelector(".popup-text").classList.toggle("popup-text-active");
+		} else if(data.target.classList[0] == "popup-text" && data.propertyName == "opacity" && data.target.classList.value.indexOf("popup-text-active") == -1) {
+			data.target.parentElement.parentElement.classList.toggle("popup-active");
+		}
+	});
+	
+	document.querySelector(".popup").addEventListener("animationend", function (data) {
+		if(data.target.classList[0] == "popup-tooltip-box") document.querySelector(".popup-tooltip-box").classList.toggle("popup-tooltip-box-active");
+	});
+	
+	/*
+		Localize Function for Language Handling
+	*/
+	const _LOCALES = ['en', 'dk'];
+	document.querySelectorAll(".language").forEach((item) => {
+		item.addEventListener("click", function (data) {
+			let language = data.target.parentElement.id.split("-")[1] 
+			if(_LOCALES.includes(language) && language != CurrentActiveLanguage) {
+				document.querySelector(".active-language").classList.toggle("active-language");
+				data.target.parentElement.classList.toggle("active-language");
+				document.querySelectorAll("body *[lang]").forEach(function (node) {
+					CurrentActiveLanguage = language;
+					if(node.lang != language) {
+					  node.style.display = 'none';
+					} else {
+					  node.style.display = 'unset';
+					}
+				});
+			}
+		});
+	});
+}
+
+/*
+	https://stackoverflow.com/questions/22119673/find-the-closest-ancestor-element-that-has-a-specific-class
+*/
+function findAncestor (el, sel) {
+    while ((el = el.parentElement) && !((el.matches || el.matchesSelector).call(el,sel)));
+    return el;
+}
---
+++ js/projects.js
@@ -0,0 +1,39 @@
+var projects = {
+	"Projects": {
+		"item-1": {
+			"name": [
+				"This Website",
+				"Denne hjemmeside"
+			],
+			"images": [
+				"images/projects/1/1.png",
+				"images/projects/1/2.png",
+				"images/projects/1/3.png"
+			],
+			"body": [
+				"<b>Libraries:</b> Vanta.js & Splide.js\n<b>Tools:</b> Figma\n\nThe website you're reading this on, the pictures are of the process from a paper prototype, to a %as[https://www.figma.com/proto/4UfbgWuP2P6tcGyNAAv18J/Portfolio-Prototype---Please-Enable-Comments-for-Developer-Thoughts?node-id=1%3A56&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A56&show-proto-sidebar=1]functional prototype on figma%ae, to the actual website itself.\n\n%lsRead More...%ae",
+				"<b>Bibloteker:</b> Vanta.js & Splide.js\n<b>Værktøjer:</b> Figma\n\nDen hjemmesider du læser dette på, billederne er af processen fra en papirs prototype, til en %as[https://www.figma.com/proto/4UfbgWuP2P6tcGyNAAv18J/Portfolio-Prototype---Please-Enable-Comments-for-Developer-Thoughts?node-id=1%3A56&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A56&show-proto-sidebar=1]funktionel prototype på figma%ae, til den aktuelle hjemmeside.\n\n%lsLæs Mere...%ae"
+			],
+			"footer": {
+				"date": "09/10-2022"
+			}
+		},
+		"item-2": {
+			"name": [
+				"Reverse Integer",
+				"Omvendt Integer"
+			],
+			"link": "https://github.com/EnSnack/JS-ReverseInteger-Fast",
+			"images": [
+				"images/projects/2/1.png"
+			],
+			"body": [
+				"Based on a challenge on leetcode, it takes a signed 32-bit integer and reverses it. The runtime of this solution in best case was anywhere between 51-68ms (99.89% at the time, 98.42% as of writing this), while the memory usage was 40.4MB (100% at the time, 100% as of writing this).\n\n%lsRead More...%ae",
+				"Baseret på en udfordring på leetcode, den tager en signed 32-bit integer og spejlvender den. Runtime på denne løsning i bedste tilfælde er et sted mellem 51-68ms (99.89% på daværende tidspunkt, 98.42% da jeg skriver dette), mens hukkomelsesbrugen var på 40.4MB (100% på daværende tidspunkt, 100% da jeg skriver dette).\n\n%lsLæs Mere...%ae"
+			],
+			"footer": {
+				"date": "09/08-2021"
+			}	
+		}
+	}
+}
