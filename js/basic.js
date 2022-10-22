999924eb071a3d693b4f53558f5ca396783e4165
EnSnack <diabloste@gmail.com>
2022-10-22 22:51:16 +0200

    Responsiveness changes, replace row with flex wrap

--- js/basic.js
+++ js/basic.js
@@ -95,12 +95,12 @@ String.prototype.format = function(o) {
 function populateProjects(json) {
 	if(Object.keys(json.Projects).length == 0) return;
 	const _FIRST_OBJECT = json.Projects[Object.keys(json.Projects)[0]]
-	if(document.querySelector("div[id^=row-]:last-child") === null || document.querySelector("div[id^=row-]:last-child").length === 0) {
+	/*if(document.querySelector("div[id^=row-]:last-child") === null || document.querySelector("div[id^=row-]:last-child").length === 0) {
 		document.querySelector("#portfolio .content").innerHTML += "<div class='row' id='row-1'></div>"
 	} else if(document.querySelectorAll("div[id^=row-]:last-child > *").length >= 4) {
 		document.querySelector("#portfolio .content").innerHTML += `<div class='row' id='row-${parseInt(document.querySelector("div[id^=row-]:last-child").id.split("-")[1])+1}'></div>`
-	}
-	const _CURRENT_ROW      = document.querySelector("div[id^=row-]:last-child")
+	}*/
+	const _CURRENT_ROW      = document.querySelector("#displayRow")
 	let _CURRENT_BOX        = `<div class='contentbox ${Object.keys(json.Projects)[0]}'></div>`
 	_CURRENT_ROW.innerHTML += _CURRENT_BOX
 	_CURRENT_BOX            = _CURRENT_ROW.querySelector(".contentbox:last-child")
